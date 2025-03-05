8945262d01922413c37bd6cfe342af19fc9c1047
Ekaterina Samarina <ekaterina.samarina@qoollo.com>
2025-03-06 00:46:32 +0300

    #17 - Сделала контекст и конфигурацию

--- src/DataAccess/BUMessenger.DataAccess.Context/BUMessengerContext.cs
+++ src/DataAccess/BUMessenger.DataAccess.Context/BUMessengerContext.cs
@@ -1,5 +1,28 @@
-﻿namespace BUMessenger.DataAccess.Context;
+﻿using BUMessenger.DataAccess.Models.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class BUMessengerContext
+namespace BUMessenger.DataAccess.Context;
+
+public class BUMessengerContext : DbContext
 {
+    public virtual DbSet<AuthTokenDb> AuthTokens { get; set; }
+    
+    public virtual DbSet<ChatDb> Chats { get; set; }
+    
+    public virtual DbSet<MessageDb> Messages { get; set; }
+    
+    public virtual DbSet<UserDb> Users { get; set; }
+    
+    public virtual DbSet<ChatUserInfoDb> ChatUserInfos { get; set; }
+    
+    public virtual DbSet<UnregisteredUserDb> UnregisteredUsers { get; set; }
+    
+    public BUMessengerContext(DbContextOptions<BUMessengerContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BUMessengerContext).Assembly);
+    }
 }
---
+++ src/DataAccess/BUMessenger.DataAccess.Context/Configuration/UserConfiguration.cs
@@ -0,0 +1,38 @@
+using BUMessenger.DataAccess.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BUMessenger.DataAccess.Context.Configuration;
+
+public class UserConfiguration : IEntityTypeConfiguration<UserDb>
+{
+    public void Configure(EntityTypeBuilder<UserDb> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id).ValueGeneratedNever();
+        
+        builder.Property(u => u.Name).IsRequired();
+        
+        builder.Property(u => u.Surname).IsRequired();
+        
+        builder.Property(u => u.FatherName).IsRequired();
+        
+        builder.Property(u => u.Gender).IsRequired();
+        
+        builder.Property(u => u.Email).IsRequired();
+        
+        builder.Property(u => u.PasswordHashed).IsRequired();
+
+        builder.HasOne(u => u.AuthToken)
+            .WithOne(a => a.User)
+            .HasForeignKey<AuthTokenDb>(a => a.UserId);
+        
+        builder.HasMany(u => u.Messages)
+            .WithOne(m => m.Creator)
+            .HasForeignKey(m => m.CreatorId);
+        
+        builder.HasMany(u => u.ChatUserInfos)
+            .WithOne(cu => cu.User)
+            .HasForeignKey(cu => cu.UserId);
+    }
+}
