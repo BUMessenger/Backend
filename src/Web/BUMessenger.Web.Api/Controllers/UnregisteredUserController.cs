32c4ba0c4d96912766d59adda393ef676312028d
Ekaterina Samarina <ekaterina.samarina@qoollo.com>
2025-03-20 12:54:44 +0300

    #29 - Добавила контроллер

---
+++ src/Web/BUMessenger.Web.Api/Controllers/UnregisteredUserController.cs
@@ -0,0 +1,33 @@
+using BUMessenger.Domain.Interfaces.Services;
+using BUMessenger.Web.Dto.Converters;
+using BUMessenger.Web.Dto.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BUMessenger.Web.Api.Controllers;
+
+[ApiController]
+[Route("/api/v1/unregistered-users")]
+public class UnregisteredUserController : ControllerBase
+{
+    private readonly IUnregisteredUserService _unregisteredUserService;
+    public UnregisteredUserController(IUnregisteredUserService unregisteredUserService)
+    {
+        _unregisteredUserService = unregisteredUserService ?? throw new ArgumentNullException(nameof(unregisteredUserService));
+    }
+    
+    [HttpPost]
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(UnregisteredUserDto), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateUnregisteredUser([FromBody] UnregisteredUserCreateDto unregisteredUserCreateDto)
+    {
+        var unregisteredUserCreateDomain = unregisteredUserCreateDto.ToDomain();
+        
+        var addedUnregisteredUser = await _unregisteredUserService.AddUnregisteredUserAsync(unregisteredUserCreateDomain);
+        
+        return StatusCode(StatusCodes.Status201Created, addedUnregisteredUser.ToDto());
+    }
+}
